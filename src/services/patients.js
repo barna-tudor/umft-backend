48476a498f8c815d3008d75d45b643d9a4c5311d
tudorc <tudor.barna01@e-uvt.ro>
2025-06-04 14:30:51 +0300

    ...missing `queries.`

--- src/services/patients.js
+++ src/services/patients.js
@@ -2,7 +2,7 @@ const expressAsyncHandler = require("express-async-handler");
 const {pool, poolQuery} = require("../database/dbConfig");
 const ErrorWrapper = require("../errorWrapper");
 const {getPublisher} = require("./redis");
-require("../database/queries/patients");
+const queries = require("../database/queries/patients");
 
 export const newPatient = expressAsyncHandler(async (req, res) => {
     let {ward_id, room_id, bed_id} = req.body;
@@ -14,15 +14,15 @@ export const newPatient = expressAsyncHandler(async (req, res) => {
     try {
         // if not provided, get the first available bed
         if (room_id == null || bed_id == null) {
-            let available_beds = (await poolQuery(getAvailableBedQuery, [ward_id])).rows[0];
+            let available_beds = (await poolQuery(queries.getAvailableBedQuery, [ward_id])).rows[0];
             if (available_beds) {
                 ({room_id, bed_id} = available_beds);
             }
         }
 
         await client.query("BEGIN");
-        const result = await client.query(insertNewPatientQuery, [ward_id, room_id, bed_id]);
-        await Promise.all([poolQuery(updateWardOccupancyQuery, [ward_id]), poolQuery(updateRoomOccupancyQuery, [room_id]), poolQuery(updateBedIsOccupiedQuery, [bed_id])]);
+        const result = await client.query(queries.insertNewPatientQuery, [ward_id, room_id, bed_id]);
+        await Promise.all([poolQuery(queries.updateWardOccupancyQuery, [ward_id]), poolQuery(queries.updateRoomOccupancyQuery, [room_id]), poolQuery(queries.updateBedIsOccupiedQuery, [bed_id])]);
         await client.query("COMMIT");
 
         return res.status(201).json({status: 201, success: true, result: {patient_id: result.rows[0].patient_id}});
